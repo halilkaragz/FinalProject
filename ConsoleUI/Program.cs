c51ce3198d7fdb3a529d9026d8f2b165dac1ebd7
halilkaragz <halilkaragoz_blsm@hotmail.com>
2021-10-24 18:39:44 +0300

    24.10.2021 2.Mola

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,12 +9,22 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //ProductTest();
+            CategoryManager categoryManager = new CategoryManager(new EFCategoryDal());
+            foreach (var item in categoryManager.GetAll())
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EFProductDal());
-            foreach (var product in productManager.GetByUnitPrice(30,100))
+            foreach (var product in productManager.GetByUnitPrice(30, 100))
             {
                 Console.WriteLine(product.ProductName);
             }
-            
         }
     }
 }
--- DataAccess/Concrete/EntityFramework/EFOrderDal.cs
+++ DataAccess/Concrete/EntityFramework/EFOrderDal.cs
@@ -1,14 +1,15 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EFCategoryDal : EfEntityRepositoryBase<Category, NorthwindContext>, ICategoryDal
-    {       
+    public class EFOrderDal:EfEntityRepositoryBase<Order, NorthwindContext>, IOrderDal
+    {
     }
 }
