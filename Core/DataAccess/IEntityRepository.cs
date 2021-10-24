361c978e03d16ff51b41729d2eaa8ccf6d866ba4
halilkaragz <halilkaragoz_blsm@hotmail.com>
2021-10-24 16:28:48 +0300

    24.10.2021 mola 1

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,12 +1,11 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //generic constraint
     //class : referans tip
--- DataAccess/Concrete/EntityFramework/EFCategoryDal.cs
+++ DataAccess/Concrete/EntityFramework/EFCategoryDal.cs
@@ -3,10 +3,7 @@ using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
