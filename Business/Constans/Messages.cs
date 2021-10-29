9623250784298cad073063ad24b98568baea131f
halilkaragz <halilkaragoz_blsm@hotmail.com>
2021-10-29 15:36:02 +0300

    10. gün 1. mola

---
+++ Business/Constans/Messages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constans
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+    }
+}
